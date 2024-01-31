41cbd76073c93c95ee38a2f15e9c8af2756ef013
Maciej Lipiński <maciej@statlook.com>
2024-01-31 15:17:06 +0100

    new packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ComixesController } from './comixes/comixes.controller';
+import { ComixesService } from './comixes/comixes.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ComixesController],
+  providers: [ComixesService],
 })
 export class AppModule {}
